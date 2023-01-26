aab967451e71267cc422ba1726e01e930659d417
JuanC Guzmán <juan.gr97@hotmail.com>
2023-01-25 21:03:36 -0500

    Frontend terminado hasta consultar la PokeAPI y mostrar la data dinamicamente

--- src/components/Chip/Chip.jsx
+++ src/components/Chip/Chip.jsx
@@ -11,7 +11,7 @@ const Chip = (props) => {
 
    useEffect(() => {
       loadDataChips();
-   }, []);
+   }, [data]);
 
 
    /* FUNCTIONS */
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import styles from "./Form.module.css";
+import PokemonContext from "../../context/pokemon/pokemonContext";
+import toast from "react-hot-toast";
 
 const Form = () => {
+
+   /* STATES */
+   const [id, setId] = useState("");
+   const [name, setName] = useState("");
+
+   /* HOOKS */
+   const pokeContext = useContext(PokemonContext);
+   const { searchPokemon } = pokeContext;
+
+   /* FUNCTIONS */
+   const handleSubmit = (ev) => {
+      ev.preventDefault();
+
+      if (id.trim() === "" && name.trim() === ""){
+         toast.error("Field ID or Name of the Pokemon is required");
+         return;
+      }
+
+      const param = (id !== "") ? id : name;
+
+      searchPokemon(param);
+   }
+
    return (
       <div className={styles.container}>
-         <form className={styles.formSearch}>
-            <input type="text" name="txtId" placeholder="ID de Pokemon"/>
-            <input type="text" name="txtName" placeholder="Nombre de Pokemon"/>
+         <form className={styles.formSearch} onSubmit={handleSubmit}>
+            <input
+               type="text"
+               name="txtId"
+               placeholder="ID de Pokemon"
+               onChange={(ev) => setId(ev.target.value)}
+            />
+            <input
+               type="text"
+               name="txtName"
+               placeholder="Nombre de Pokemon"
+               onChange={(ev) => setName(ev.target.value)}
+            />
             <div>
                <button type="submit">
                   SEARCH
